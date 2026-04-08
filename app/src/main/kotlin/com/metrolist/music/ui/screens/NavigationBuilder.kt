6514002253cd50612398e570b0ca720eed50ba9d
Nicholas DellaGrotte <ndell1@unh.newhaven.edu>
2026-04-07 21:11:34 -0400

    refine navigation for EQ wizard back stack
    Updates the `eq_wizard` route to explicitly pop the back stack to the `equalizer` screen upon completion

--- app/src/main/kotlin/com/metrolist/music/ui/screens/NavigationBuilder.kt
+++ app/src/main/kotlin/com/metrolist/music/ui/screens/NavigationBuilder.kt
@@ -432,7 +432,7 @@ fun NavGraphBuilder.navigationBuilder(
     }
 
     composable("eq_wizard") {
-        WizardScreen(onNavigateBack = { navController.popBackStack() })
+        WizardScreen(onNavigateBack = { navController.popBackStack("equalizer", inclusive = false) })
     }
 
     composable(
